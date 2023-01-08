ff018ad49e7994ca30433c491d65b9b8ca34fa7d
Gauri Mahashabde <gauri2interact@gmail.com>
2023-01-08 21:43:51 +0530

    solution 2

--- easy_problems/array/string/Richestcustomer.js
+++ easy_problems/array/string/Richestcustomer.js
@@ -1,3 +1,18 @@
 //leetcode.com/problems/richest-customer-wealth/
 
-const maximumWealth = accounts =>  Math.max(...accounts.map(acc => acc.reduce((prev, cur) => prev + cur, 0)));
+//solution 1
+const maximumWealth = (accounts) =>
+  Math.max(...accounts.map((acc) => acc.reduce((prev, cur) => prev + cur, 0)));
+
+//solution 2
+var maximumWealth1 = function (accounts) {
+  let m = 0;
+  for (var i = 0; i < accounts.length; i++) {
+    var s = 0;
+    for (var j = 0; j < accounts[i].length; j++) {
+      s += accounts[i][j];
+    }
+    if (s > m) m = s;
+  }
+  return m;
+};
