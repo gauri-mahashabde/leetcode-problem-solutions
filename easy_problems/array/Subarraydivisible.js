971f9308aab0f1206c96d954c8b0ed5c5db20fad
Gauri Mahashabde <gauri2interact@gmail.com>
2023-01-21 21:41:20 +0530

    New leetcode probelem added

---
+++ easy_problems/array/Subarraydivisible.js
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/subarray-sums-divisible-by-k/solution/
+
+var subarraysDivByK = function(nums, k) {
+    let map ={}
+    map[0] = 1;
+    let count = 0;
+    let total = 0;
+
+    for(let num of nums){
+        count = (count+num) %k;
+        if(count<0) count +=k;
+        if(map[count]) total += map[count]
+        map[count] = map[count] ? map[count] + 1 : 1
+    }
+    return total;
+}
