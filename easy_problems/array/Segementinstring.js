1b2e2b64160d01bd085b99d02c93931cbb6a8107
Gauri Mahashabde <gauri2interact@gmail.com>
2023-01-28 22:05:23 +0530

    solution 2 added

---
+++ easy_problems/array/Segementinstring.js
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/number-of-segments-in-a-string/
+
+let countSegments = s => s.split(" ").filter(s => s.trim().length).length;
+
+var countSegments1 = function(s) {
+    let hasSpace = true;
+    let count = 0;
+  
+    for (const char of s) {
+      if (hasSpace && char !== " ") {
+        count++;
+        hasSpace = false;
+      } else if (!hasSpace && char === " ") {
+        hasSpace = true;
+      }
+    }
+  
+    return count;
+  };
