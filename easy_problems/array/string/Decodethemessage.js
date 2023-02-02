f6cf5b864e65ce36563bf7308e45be4a057c9ebf
Gauri Mahashabde <gauri2interact@gmail.com>
2023-02-02 21:51:45 +0530

    Leetcode probelem solution 3

--- easy_problems/array/string/Decodethemessage.js
+++ easy_problems/array/string/Decodethemessage.js
@@ -27,3 +27,31 @@ var decodeMessage = function(key, message) {
     for (let i of message) str+=a[alphabet.indexOf(i)]
     return str
 }
+
+
+const decodeMessage = (key, message) => {
+    const map = new Map();
+    const uniques = [];
+  
+    for (let i = 0; i < key.length; i += 1) {
+      if (key[i] !== ' ' && !map.has(key[i])) {
+        map.set(key[i], '');
+        uniques.push(key[i]);
+      }
+    }
+  
+    const firstCharCode = 'a'.charCodeAt(0); // 97
+  
+    for (let i = 0; i < uniques.length; i += 1) {
+      const code = uniques[i].charCodeAt();
+      map.set(uniques[i], String.fromCharCode(i + firstCharCode));
+    }
+  
+    let result = '';
+  
+    for (let i = 0; i < message.length; i += 1) {
+      result += map.get(message[i]) ?? ' ';
+    }
+  
+    return result;
+  };
