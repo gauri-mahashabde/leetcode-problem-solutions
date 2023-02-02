6b8694c633717d8b424c10ef002d7306e1f4c042
Gauri Mahashabde <gauri2interact@gmail.com>
2023-02-02 21:47:21 +0530

    Leetcode probelem solutin 1

--- easy_problems/array/string/Decodethemessage.js
+++ easy_problems/array/string/Decodethemessage.js
@@ -1,2 +1,16 @@
 //https://leetcode.com/problems/decode-the-message/
 
+//solution 1
+
+var decodeMessage = function(key, message) {
+    let keySet = Array.from(new Set(key.split(' ').join('')));
+    for (let i = 0; i < alpha.length; i++) {
+        hash.set(keySet[i], alpha[i])
+    }
+
+    for (let chr of message) {
+        result += hash.get(chr) || ' '
+    }
+
+    return result
+};
