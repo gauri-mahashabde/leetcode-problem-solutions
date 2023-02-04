8868c620d626bbea8f64ca661cdf6fbd66ebde02
Gauri Mahashabde <gauri2interact@gmail.com>
2023-02-04 14:52:42 +0530

    Leetcode problem Solution added

--- easy_problems/array/string/Tolowercase.js
+++ easy_problems/array/string/Tolowercase.js
@@ -1,1 +1,18 @@
 //https://leetcode.com/problems/to-lower-case/
+
+var toLowerCase = function(str) {
+    let res = "";
+    
+    for (let i = 0; i < str.length; i++) {
+        let ascii = str.charCodeAt(i);
+		
+		// The ascii value from 65 to 90 represent the uppercase alphabet A to Z.
+		// While, the ascii value from 97 to 122 represent the lowercase alphabet a to z.
+		// By adding 32 to the ascii value, we are converting it to lowercase.
+        if (ascii >= 65 && ascii <= 90) res += String.fromCharCode(ascii + 32);
+        else res += str.charAt(i);
+    }
+    
+    return res;
+};
+
