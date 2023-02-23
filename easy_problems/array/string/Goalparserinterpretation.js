84b37a2701323fd1144ae8d49a68ff2bc4dc91de
Gauri Mahashabde <gauri2interact@gmail.com>
2023-02-23 18:54:05 +0530

    Solution 1 added

--- easy_problems/array/string/Goalparserinterpretation.js
+++ easy_problems/array/string/Goalparserinterpretation.js
@@ -1,1 +1,8 @@
 //https://leetcode.com/problems/goal-parser-interpretation/
+
+const interpret = function (command) {
+  let rpal = command.split("()").join("o");
+  let x = rpal.split("(al)").join("al");
+  return x;
+};
+
