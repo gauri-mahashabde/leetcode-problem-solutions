b7713c882b3348c39823cd3945b852480c12d003
Gauri Mahashabde <gauri2interact@gmail.com>
2023-02-28 12:33:07 +0530

    Leetcode solution 1 added

--- easy_problems/array/string/Eqivalentstringarray.js
+++ easy_problems/array/string/Eqivalentstringarray.js
@@ -1,1 +1,17 @@
 //https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+//solution 1
+
+const arrayStringsAreEqual = function (word1, word2) {
+  const stringword1 = word1.toString();
+  const stringword2 = word2.toString();
+
+  const string1 = stringword1.split(",").join("");
+  const string2 = stringword2.split(",").join("");
+
+  if (string1 === string2) {
+    return true;
+  } else {
+    return false;
+  }
+};
