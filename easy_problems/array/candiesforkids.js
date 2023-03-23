cb2b2740ca2d95f24903e191e005186e04a3dc4a
Gauri Mahashabde <gauri2interact@gmail.com>
2023-03-23 23:05:44 +0530

    leetcode problem added

--- easy_problems/array/candiesforkids.js
+++ easy_problems/array/candiesforkids.js
@@ -1,1 +1,16 @@
 //https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+const kidsWithCandies = function (candies, extraCandies) {
+    let a = Math.max(...candies);
+    let b = [];
+  
+    for (let i = 0; i < candies.length; i++) {
+      if (candies[i] + extraCandies >= a) {
+        b.push(true);
+      } else {
+        b.push(false);
+      }
+    }
+  
+    return b;
+  };
