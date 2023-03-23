696f6a4b0e516763489cf31a5f8523bbd42a9519
Gauri Mahashabde <gauri2interact@gmail.com>
2023-03-23 22:09:10 +0530

    leetcode problem added

--- easy_problems/array/Buildarraypermutation.js
+++ easy_problems/array/Buildarraypermutation.js
@@ -2,16 +2,16 @@
 
 //solution 1
 
-const buildArray = function(nums) {
-    return nums.map(a=>nums[a]);
+const buildArray = function (nums) {
+  return nums.map((a) => nums[a]);
 };
 
 //solution 2
 
-const buildArray1 = function(nums) {
-    arr = []
-    for(let i=0; i<nums.length; i++){
-        arr.push(nums[nums[i]])
-    }
-    return arr
- };
+const buildArray1 = function (nums) {
+  arr = [];
+  for (let i = 0; i < nums.length; i++) {
+    arr.push(nums[nums[i]]);
+  }
+  return arr;
+};
