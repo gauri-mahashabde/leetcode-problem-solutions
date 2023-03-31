113ee8cd763c87f1def18ceb5cf7961b261ac16d
Gauri Mahashabde <gauri2interact@gmail.com>
2023-03-31 20:53:57 +0530

    solution 1 added

--- easy_problems/array/decompress.js
+++ easy_problems/array/decompress.js
@@ -1,2 +1,20 @@
 //https://leetcode.com/problems/decompress-run-length-encoded-list/
 
+//solution 1
+
+const decompressRLElist = function (nums) {
+  let result = [];
+  let i = 0,
+    j = 1;
+
+  while (j < nums.length) {
+    let freq = nums[i];
+    for (let count = 0; count < freq; ++count) {
+      result.push(nums[j]);
+    }
+    i += 2;
+    j += 2;
+  }
+
+  return result;
+};
