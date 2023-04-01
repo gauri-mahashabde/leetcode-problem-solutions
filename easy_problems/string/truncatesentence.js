6d192695209cc8f07bc0a25855e324169ad8666c
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-01 18:52:36 +0530

    Solution 2 added

--- easy_problems/string/truncatesentence.js
+++ easy_problems/string/truncatesentence.js
@@ -9,3 +9,15 @@ const truncateSentence = (s, k) => {
 
   return array.join(" ");
 };
+
+//solution 2
+
+const truncateSentence1 = function (s, k) {
+  let str = "";
+  var word = s.split(" ");
+  for (let i = 0; i < k - 1; i++) {
+    str += word[i] + " ";
+  }
+  str += word[k - 1];
+  return str;
+};
