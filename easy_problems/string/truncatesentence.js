125e8792c21138c494fab73fc97db474a6a9a457
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-01 18:51:09 +0530

    Solution 1 added

--- easy_problems/string/truncatesentence.js
+++ easy_problems/string/truncatesentence.js
@@ -1,2 +1,11 @@
 //https://leetcode.com/problems/truncate-sentence/
 
+//solution 1
+
+const truncateSentence = (s, k) => {
+  const array = s.split(" ");
+
+  array.length = k;
+
+  return array.join(" ");
+};
