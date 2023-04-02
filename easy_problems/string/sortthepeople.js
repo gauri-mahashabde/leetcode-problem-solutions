50956ee5b3d9142ea2d7b4a977148842ed8c5cd2
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-02 09:51:39 +0530

    solution 1 added

--- easy_problems/string/sortthepeople.js
+++ easy_problems/string/sortthepeople.js
@@ -1,1 +1,12 @@
 //https://leetcode.com/problems/sort-the-people/
+
+//solution 1
+
+const sortPeople = function(names, heights) {
+    let finalArray = []
+    for(let i=0;i<names.length;i++){
+        finalArray.push([names[i],heights[i]])
+    }
+    return finalArray.sort((a,b)=>b[1]-a[1]).map((a)=>a[0]);
+    
+};
