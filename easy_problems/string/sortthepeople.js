252ba9436171c0a8bcd72d36fda8b281002320d1
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-02 09:53:42 +0530

    Solution 2 added

--- easy_problems/string/sortthepeople.js
+++ easy_problems/string/sortthepeople.js
@@ -2,11 +2,18 @@
 
 //solution 1
 
-const sortPeople = function(names, heights) {
-    let finalArray = []
-    for(let i=0;i<names.length;i++){
-        finalArray.push([names[i],heights[i]])
-    }
-    return finalArray.sort((a,b)=>b[1]-a[1]).map((a)=>a[0]);
+const sortPeople = function (names, heights) {
+  let finalArray = [];
+  for (let i = 0; i < names.length; i++) {
+    finalArray.push([names[i], heights[i]]);
+  }
+  return finalArray.sort((a, b) => b[1] - a[1]).map((a) => a[0]);
+};
+
+//solution 2
+
+var sortPeople1 = function(names, heights) {
+    const people = names.map((name, index) => ({name, height: heights[index]}));
     
+    return people.sort((a, b) => b.height - a.height).map(({name}) => name);
 };
