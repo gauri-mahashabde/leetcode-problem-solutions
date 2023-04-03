2cd0569076c66c8cf029ad91a08a6326d1be32dd
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-03 09:42:24 +0530

    Solution 1 added

--- easy_problems/array/shufflethearray.js
+++ easy_problems/array/shufflethearray.js
@@ -1,3 +1,12 @@
 //https://leetcode.com/problems/shuffle-the-array/
 
 //solution
+const shuffle = function (nums, n) {
+  let res = [];
+
+  for (let i = 0; i < nums.length / 2; i++) {
+    res.push(nums[i], nums[i + n]);
+  }
+
+  return res;
+};
