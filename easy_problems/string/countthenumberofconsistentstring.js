13cbd47ce7ea9fa368ea65fcb89af3ca244e440e
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-07 08:10:32 +0530

    solution 2 added

--- easy_problems/string/countthenumberofconsistentstring.js
+++ easy_problems/string/countthenumberofconsistentstring.js
@@ -1,1 +1,17 @@
 //https://leetcode.com/problems/count-the-number-of-consistent-strings/
+
+//solution 1
+
+const countConsistentStrings = function (allowed, words) {
+  return words.filter((x) =>
+    x.split("").every((x) => allowed.split("").includes(x))
+  ).length;
+};
+
+//solution 2
+const countConsistentStrings1 = (allowed, words) => {
+  let set = new Set(allowed);
+  return words.reduce((a, w) => {
+    return w.split("").every((l) => set.has(l)) ? ++a : a;
+  }, 0);
+};
