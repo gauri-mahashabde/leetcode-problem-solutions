cea47a72cf79b2a2dc73e3a72cbb216b57d56810
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-08 21:07:07 +0530

    Solution 2 added

--- easy_problems/array/RemoveDuplicate.js
+++ easy_problems/array/RemoveDuplicate.js
@@ -2,17 +2,29 @@
 
 //solution 1
 
-const removeDuplicates = function(nums) {
-    if (nums.length === 0) return 0;
-    
-    let i = 0;
-    
-    for (let j = 1; j < nums.length; j++) {
-        if (nums[j] !== nums[i]) {
-            i++;
-            nums[i] = nums[j];
-        }
+const removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+
+  let i = 0;
+
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
     }
-    
-    return i + 1;
+  }
+
+  return i + 1;
+};
+
+//solution 2
+
+const removeDuplicates1 = (nums) => {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] == nums[i + 1]) {
+      nums.splice(i + 1, 1);
+      i--;
+    }
+  }
+  return nums.length;
 };
