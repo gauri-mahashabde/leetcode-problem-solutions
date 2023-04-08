266c4b2129cb13c3465e5f4c6c6f7f3801f3dd3a
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-08 21:02:47 +0530

    Solution 1 added

--- easy_problems/array/RemoveDuplicate.js
+++ easy_problems/array/RemoveDuplicate.js
@@ -1,1 +1,18 @@
 //https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+//solution 1
+
+const removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+    
+    let i = 0;
+    
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    
+    return i + 1;
+};
