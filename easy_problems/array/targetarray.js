e865669da9c81cd95a96b5ff230a2cb62aa3b292
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-08 21:01:31 +0530

    New leetcode problem added

--- easy_problems/array/targetarray.js
+++ easy_problems/array/targetarray.js
@@ -1,1 +1,28 @@
 //https://leetcode.com/problems/create-target-array-in-the-given-order/
+
+//solution 1
+const createTargetArray = (nums, idx) => {
+  let target = [];
+  for (const i in nums) target.splice(idx[i], 0, nums[i]);
+  return target;
+};
+
+//solution 2
+const createTargetArray1 = function (nums, index) {
+  let number = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (number[index[i]] === undefined) {
+      number[index[i]] = nums[index[i]];
+      continue;
+    }
+
+    for (j = number.length; j > index[i]; j--) {
+      number[j] = number[j - 1];
+    }
+
+    number[index[i]] = nums[i];
+  }
+
+  return number;
+};
