f807973ddf5241b6f1e1567355095b7d9ead3358
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-09 09:31:11 +0530

    solution 2 added

--- easy_problems/array/sumofoddarrayallsubarray.js
+++ easy_problems/array/sumofoddarrayallsubarray.js
@@ -14,3 +14,27 @@ const sumOddLengthSubarrays = function (arr) {
 
   return output;
 };
+
+//solution 2
+
+var sumOddLengthSubarrays1 = function (arr) {
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    // helping in selecting the starting point
+
+    for (let j = i; j < arr.length; j++) {
+      // helping in selecting the endpoint point
+
+      if ((i - j) % 2 == 0) {
+        // so that it only count the subarrays which have odd length
+
+        for (let k = i; k <= j; k++) {
+          // looping from start point to end point and adding them
+          count += arr[k];
+        }
+      }
+    }
+  }
+  return count;
+};
