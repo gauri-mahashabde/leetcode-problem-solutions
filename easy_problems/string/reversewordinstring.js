b59d1455f4559095da107771f4dd2ac7d0194148
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-10 19:24:09 +0530

    Solution 1 added

--- easy_problems/string/reversewordinstring.js
+++ easy_problems/string/reversewordinstring.js
@@ -2,20 +2,21 @@
 
 //solution 1
 const reverseWords = function (s) {
-  let finalStr = "";
-  let subString = "";
-  for (let i = 0; i < s.length; i++) {
-    if (s.charAt(i) === " " || s.length === i + 1) {
-      if (s.length === i + 1) {
-        subString = s.charAt(i) + subString;
-      } else {
-        subString = subString + s.charAt(i);
+    let finalStr = "";
+    let subString = "";
+    for (let i = 0; i < s.length; i++) {
+      if (s.charAt(i) === " " || s.length === i + 1) {
+        if (s.length === i + 1) {
+          subString = s.charAt(i) + subString;
+        } else {
+          subString = subString + s.charAt(i);
+        }
+        finalStr += subString;
+        subString = "";
+        continue;
       }
-      finalStr += subString;
-      subString = "";
-      continue;
+      subString = s.charAt(i) + subString;
     }
-    subString = s.charAt(i) + subString;
-  }
-  return finalStr;
-};
+    return finalStr;
+  };
+  
