d2f5fbeaa1cd1b666cb80bc6a8eddaa7760913b1
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-10 19:26:33 +0530

    Solution 2 added

--- easy_problems/string/reversewordinstring.js
+++ easy_problems/string/reversewordinstring.js
@@ -2,21 +2,41 @@
 
 //solution 1
 const reverseWords = function (s) {
-    let finalStr = "";
-    let subString = "";
-    for (let i = 0; i < s.length; i++) {
-      if (s.charAt(i) === " " || s.length === i + 1) {
-        if (s.length === i + 1) {
-          subString = s.charAt(i) + subString;
-        } else {
-          subString = subString + s.charAt(i);
-        }
-        finalStr += subString;
-        subString = "";
-        continue;
+  let finalStr = "";
+  let subString = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i) === " " || s.length === i + 1) {
+      if (s.length === i + 1) {
+        subString = s.charAt(i) + subString;
+      } else {
+        subString = subString + s.charAt(i);
       }
-      subString = s.charAt(i) + subString;
+      finalStr += subString;
+      subString = "";
+      continue;
     }
-    return finalStr;
-  };
-  
+    subString = s.charAt(i) + subString;
+  }
+  return finalStr;
+};
+
+//solution 2
+var reverseWords1 = function (s) {
+  let arrOfWords = s.split(" ");
+
+  for (let i = 0; i < arrOfWords.length; ++i) {
+    let eachWord = arrOfWords[i].split("");
+    let left = 0,
+      right = eachWord.length - 1;
+    while (left < right) {
+      let temp = eachWord[right];
+      eachWord[right] = eachWord[left];
+      eachWord[left] = temp;
+      left++;
+      right--;
+    }
+    arrOfWords[i] = eachWord.join("");
+  }
+
+  return arrOfWords.join(" ");
+};
