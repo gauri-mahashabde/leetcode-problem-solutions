8d4a02b0337ecc565a14a5840038a02d385b6120
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-10 19:22:10 +0530

    solution 1 added

--- easy_problems/string/reversewordinstring.js
+++ easy_problems/string/reversewordinstring.js
@@ -1,1 +1,21 @@
 //https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+//solution 1
+const reverseWords = function (s) {
+  let finalStr = "";
+  let subString = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i) === " " || s.length === i + 1) {
+      if (s.length === i + 1) {
+        subString = s.charAt(i) + subString;
+      } else {
+        subString = subString + s.charAt(i);
+      }
+      finalStr += subString;
+      subString = "";
+      continue;
+    }
+    subString = s.charAt(i) + subString;
+  }
+  return finalStr;
+};
