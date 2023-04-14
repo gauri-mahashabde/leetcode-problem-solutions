26707a24adc06712c57d5c8bab163690f16f5402
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-14 19:56:12 +0530

    Solution 1 added

--- easy_problems/JS/filterelementsfromanarray.js
+++ easy_problems/JS/filterelementsfromanarray.js
@@ -1,1 +1,13 @@
 //https://leetcode.com/problems/filter-elements-from-array/
+
+//Solution 1
+
+const filter = function (arr, fn) {
+  const res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      res.push(arr[i]);
+    }
+  }
+  return res;
+};
