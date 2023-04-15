abd2d1b98c7dc0f53dd651771b01ce42e5d6be99
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-15 21:08:12 +0530

    solution 1 added

--- easy_problems/array/matrixdiagonalsum.js
+++ easy_problems/array/matrixdiagonalsum.js
@@ -1,1 +1,13 @@
 //https://leetcode.com/problems/matrix-diagonal-sum/
+
+//solution 1
+const diagonalSum = function (mat) {
+  let sum = 0;
+  for (let i = 0; i < mat.length; i++) {
+    sum += mat[i][i];
+    if (mat.length - 1 - i != i) {
+      sum += mat[i][mat.length - 1 - i];
+    }
+  }
+  return sum;
+};
