7e8abedb4eaba7fb235e3110fa3c59700b3e7e2c
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-16 21:15:33 +0530

    solution 1 added

--- easy_problems/string/replacealldigitswithcharacters.js
+++ easy_problems/string/replacealldigitswithcharacters.js
@@ -1,1 +1,18 @@
 //https://leetcode.com/problems/replace-all-digits-with-characters/
+
+//solution 1
+const replaceDigits = function (s) {
+  let d = "";
+
+  for (let i of s) {
+    if (!isNaN(i)) {
+      console.log("i is", i);
+      let z = d.charCodeAt(d.length - 1) + parseInt(i);
+      d += String.fromCharCode(z);
+    } else {
+      d += i;
+      console.log("else d is", d);
+    }
+  }
+  return d;
+};
