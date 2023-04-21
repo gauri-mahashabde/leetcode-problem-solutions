93db0a326b6b05188a38f643a64b27609260541c
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-21 19:06:13 +0530

    solution 2 added

--- easy_problems/string/maximumnumberofwords.js
+++ easy_problems/string/maximumnumberofwords.js
@@ -4,9 +4,15 @@
 
 const mostWordsFound = function (sentences) {
   let max = 0;
-  for (let i = 0; i      < sentences.length; i++) {
+  for (let i = 0; i < sentences.length; i++) {
     let temp = sentences[i].split(" ").length;
     if (temp > max) max = temp;
   }
   return max;
+};
+
+//solution 2
+
+const mostWordsFound1 = function (sentences) {
+  return Math.max(...sentences.map((item) => item.split(" ").length));
 };
