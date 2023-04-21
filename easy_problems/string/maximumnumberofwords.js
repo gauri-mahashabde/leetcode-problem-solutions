2a370cccb635979c9ab11b84baa36079a0c947ce
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-21 19:03:52 +0530

    solution 1 added

--- easy_problems/string/maximumnumberofwords.js
+++ easy_problems/string/maximumnumberofwords.js
@@ -1,2 +1,12 @@
 //https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
 
+//solution 1
+
+const mostWordsFound = function (sentences) {
+  let max = 0;
+  for (let i = 0; i      < sentences.length; i++) {
+    let temp = sentences[i].split(" ").length;
+    if (temp > max) max = temp;
+  }
+  return max;
+};
