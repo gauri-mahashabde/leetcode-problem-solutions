253f84097bd3baad1dd1febd50853bf15d649267
Gauri Mahashabde <gauri2interact@gmail.com>
2023-06-04 22:01:04 +0530

    new problem added

--- easy_problems/JS/functioncomposition.js
+++ easy_problems/JS/functioncomposition.js
@@ -1,1 +1,11 @@
 //https://leetcode.com/problems/function-composition/
+
+var compose = function(functions) {
+	return function(x) {
+       let ans =  x;
+      for(let i=functions.length-1;i>=0;i--){
+          ans = functions[i](ans)
+      }
+      return ans
+    }
+};
