71d1320fce1f5c5b2378135fee606ee30a97d424
Gauri Mahashabde <gauri2interact@gmail.com>
2023-06-12 22:01:58 +0530

    leetcode problem added

--- easy_problems/array/summultiples.js
+++ easy_problems/array/summultiples.js
@@ -13,3 +13,16 @@ const sumOfMultiples = function (n) {
 
   return sum;
 };
+
+//solution 2
+const sumOfMultiples1 = function (n) {
+  var i = 1,
+    sum = 0;
+  while (i <= n) {
+    if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+      sum = sum + i;
+    }
+    i++;
+  }
+  return sum;
+};
