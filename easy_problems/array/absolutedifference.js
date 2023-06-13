c0df148786f6cf5008d7b706f8817d88d8e1952c
Gauri Mahashabde <gauri2interact@gmail.com>
2023-06-13 21:30:11 +0530

    new problem added

--- easy_problems/array/absolutedifference.js
+++ easy_problems/array/absolutedifference.js
@@ -1,2 +1,12 @@
 //https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/
 
+//solution 1
+var countKDifference = function(nums, k) {
+    let finalArr = []
+    for(let i=0; i<nums.length; i++) {
+        for(let j=i+1; j<nums.length; j++) {
+            Math.abs(nums[i]-nums[j]) === k && finalArr.push({i,j})
+        }
+    }
+    return finalArr.length
+};
