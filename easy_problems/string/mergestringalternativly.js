9814097ddbc570e3f567c289fcf78166be6b9845
Gauri Mahashabde <gauri2interact@gmail.com>
2023-06-16 23:42:46 +0530

    new string problem added

--- easy_problems/string/mergestringalternativly.js
+++ easy_problems/string/mergestringalternativly.js
@@ -1,1 +1,15 @@
 //https://leetcode.com/problems/merge-strings-alternately/
+
+//solution 1
+
+const mergeAlternately = function (word1, word2) {
+  let result = [];
+  let maxLength = Math.max(word1.length, word2.length);
+
+  for (let i = 0; i < maxLength; i++) {
+    if (word1[i]) result.push(word1[i]);
+    if (word2[i]) result.push(word2[i]);
+  }
+
+  return result.join("");
+};
