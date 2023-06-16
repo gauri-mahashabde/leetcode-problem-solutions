cd6d79e184dccaea7acba6a2614c87f73e752c8c
Gauri Mahashabde <gauri2interact@gmail.com>
2023-06-16 23:45:33 +0530

    new string problem added

--- easy_problems/string/mergestringalternativly.js
+++ easy_problems/string/mergestringalternativly.js
@@ -13,3 +13,24 @@ const mergeAlternately = function (word1, word2) {
 
   return result.join("");
 };
+
+//solution 2
+
+const mergeAlternately1 = (word1, word2) => {
+  let arr = [];
+  let n = Math.min(word1.length, word2.length);
+
+  for (let i = 0; i < n; i++) {
+    arr.push(word1[i], word2[i]);
+  }
+
+  if (word1.length > n) {
+    arr.push(...word1.slice(n));
+  }
+
+  if (word2.length > n) {
+    arr.push(...word2.slice(n));
+  }
+
+  return arr.join("");
+};
