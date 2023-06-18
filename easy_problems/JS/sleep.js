193d30b2e19f15df48163f4454fa39c17d9225ca
Gauri Mahashabde <gauri2interact@gmail.com>
2023-06-18 19:23:51 +0530

    Problem added

--- easy_problems/JS/sleep.js
+++ easy_problems/JS/sleep.js
@@ -1,5 +1,19 @@
 //https://leetcode.com/problems/sleep/
 
+//solution 1
+
 async function sleep(millis) {
     return new Promise((res) => setTimeout(res, millis))
 }
+
+//solution 2
+
+async function sleep(millis) {
+    return new Promise((delayresolve, reject) => {
+      if (typeof millis !== 'number' || isNaN(millis)) {
+        reject(new Error('Invalid argument. Expected a number.'));
+      } else {
+        setTimeout(delayresolve, millis);
+      }
+    });
+  }
