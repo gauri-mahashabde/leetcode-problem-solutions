786441c584f5f8f9c2f47ec7efbe0d76ce32690f
Gauri Mahashabde <gauri2interact@gmail.com>
2023-06-22 19:06:12 +0530

    New problem added

--- easy_problems/array/fourdigitpossiblesum.js
+++ easy_problems/array/fourdigitpossiblesum.js
@@ -1,1 +1,20 @@
 //https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
+
+//solution 1
+var minimumSum = function(num) {
+    let arr = [];
+    num = num.toString();
+    
+    for (let i=0;i<4;i++) {
+        arr.push(parseInt(num[i]))
+    }
+    
+    arr.sort();
+    
+    let new1 = arr[0].toString() + arr[2].toString();  
+    let new2 = arr[1].toString() + arr[3].toString();
+    
+    return parseInt(new1) + parseInt(new2);
+};
+
+//solution 2
