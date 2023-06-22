f0c2d792a5467db0bcecc3d97f61d6d313f7d50b
Gauri Mahashabde <gauri2interact@gmail.com>
2023-06-22 19:06:59 +0530

    New problem added

--- easy_problems/array/fourdigitpossiblesum.js
+++ easy_problems/array/fourdigitpossiblesum.js
@@ -18,3 +18,14 @@ var minimumSum = function(num) {
 };
 
 //solution 2
+var minimumSum = function(num) {
+    let result = 0
+    String(num).split("").sort().forEach((item,index) => {
+        if(index === 0 | index === 1) {
+            result += parseInt(item) * 10
+            return
+        }
+        result += parseInt(item)
+    })
+    return result
+};
